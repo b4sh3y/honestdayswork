bf4624dfe1c65e6d985bca81423d02f5d95d54c7
Valcsak <balazs.valcsak@evosoft.com>
2023-08-30 12:18:07 +0200

    Website availability check and testplan draft have been added

---
+++ Automated tests/SportradarWebsiteAvailabilityTest.js
@@ -0,0 +1,27 @@
+const { Builder, By } = require('selenium-webdriver');
+const assert = require('assert');
+
+describe('Website Availability Test', function () {
+  this.timeout(50000); // Set a longer timeout if needed
+
+  let driver;
+
+  before(async function () {
+    driver = new Builder().forBrowser('chrome').build();
+  });
+
+  after(async function () {
+    await driver.quit();
+  });
+
+  it('isAvailable', async function () {
+    const websiteUrl = 'https://www.sportradar.com';
+
+    await driver.get(websiteUrl);
+
+    const errorElements = await driver.findElements(By.css('.error-class'));
+    const isWebsiteAvailable = errorElements.length === 0;
+
+    assert.strictEqual(isWebsiteAvailable, true, 'Website is not available.');
+  });
+});
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # honestdayswork
 Showcase of my stack knowledege
+
+DRAFT
+
+Test cases
+Hover over button test
+- Check if background changes
+Contact us test
+- Check if warning comes when button is clicked but the required fields are left blank
+- Check if data disappears from filled out fields when button is clicked 
+Career site filter bar test
+- Check if the drop down menu opens when location button is clicked
+- Check if data is filtered when clicking the "Employees can work remotely" button
+
+Dependencies
+- node.js
+- selenium webdriver mocha
